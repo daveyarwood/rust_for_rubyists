0ec39bd90d4a36690aaf368abb5e54553c092e63
Dave Yarwood <dave.yarwood@gmail.com>
2016-02-01 09:57:17 -0500

    update 03 and 04 to rust 1.5.0

--- 03-testing.rs
+++ 03-testing.rs
@@ -1,4 +1,12 @@
+/*
+  1.5.0: Tests now require a `main` function, even if it's not doing anything.
+
+  Running `rustc 03-testing.rs --test && ./03-testing` still works.
+*/
+
 #[test]
 fn this_tests_code() {
   println!("");
 }
+
+fn main() {}
--- 04-forced-failure.rs
+++ 04-forced-failure.rs
@@ -1,9 +1,17 @@
-/* 
+/*
+  1.0.0:
+  -----
   failing is now called panicking
   fail!() -> panic!()
+
+  1.5.0:
+  -----
+  - tests now require a `main` function
 */
 
 #[test]
 fn this_tests_code() {
   panic!("Fail!");
 }
+
+fn main() {}
