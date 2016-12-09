d75abbe29f87cbcb5768d19567ddad8967be4da1
Dave Yarwood <dave.yarwood@gmail.com>
2016-12-09 08:32:55 -0500

    update 06 for 1.9.0

--- 06-500-hellos.rs
+++ 06-500-hellos.rs
@@ -1,5 +1,13 @@
+/*
+  1.9.0:
+  -----
+  1. `u` is no longer a valid suffix for a numeric literal
+  2. `range` is no longer a thing. now there is a literal range syntax
+  3. unused variables cause a warning; use _
+*/
+
 fn main() {
-  for num in range(0u, 500) {
+  for _ in 0..500 {
     println!("Hello");
   }
 }
