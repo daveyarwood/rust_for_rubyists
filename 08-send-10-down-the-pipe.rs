64afd9e4176be301c3eedd216542038c5b5478c7
Dave Yarwood <dave.yarwood@gmail.com>
2016-12-09 08:37:04 -0500

    update 08 for rust 1.9.0

--- 08-send-10-down-the-pipe.rs
+++ 08-send-10-down-the-pipe.rs
@@ -1,15 +1,22 @@
-use std::thread::Thread;
+use std::thread;
 use std::sync::mpsc::channel;
 
 /*
+  1.0.0:
+  -----
   1. channel() now lives in std::sync::mpsc
   2. unwrap() is now a thing -- without it, you get a Result ("Ok(10)")
+
+  1.9.0:
+  -----
+  1. 'u' suffix is no longer valid
+  4. std::thread::Thread is now just std::thread
 */
 
 fn main() {
   let (chan, port) = channel();
 
-  Thread::spawn(move || chan.send(10u));
+  thread::spawn(move || chan.send(10));
 
   println!("{}", port.recv().unwrap().to_string());
 }
