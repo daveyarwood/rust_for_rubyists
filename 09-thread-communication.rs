78b5cb6d86017b70802ebb953ed593ce8892549b
Dave Yarwood <dave.yarwood@gmail.com>
2016-12-09 08:48:08 -0500

    updated 09 for rust 1.9.0 and now it magically works :confetti_ball:

--- 09-thread-communication.rs
+++ 09-thread-communication.rs
@@ -1,48 +1,42 @@
-use std::thread::Thread;
+use std::thread;
 use std::sync::mpsc::{channel, Sender, Receiver};
 
 /*
-  Like channel, Sender and Receiver now live in std::sync::mpsc.
-
-  Unresolved -- this file will compile, but the receiver blocks while
-  waiting for a value. Documentation on the Rust site (the Rust book)
-  seems to be out-of-date for the nightly build. I can't figure out
-  how to get plus_one to run as an asynchronous, non-blocking thread.
+  1.9.0:
+  -----
+  1. Like channel, Sender and Receiver now live in std::sync::mpsc.
+  2. u is no longer a valid numeric literal suffix
+  3. `int` is not a valid type; must use isize, i32, i64, etc.
+  4. `range` is no longer a thing. now there is a literal range syntax
+  5. unused variables cause a warning; use _
+  6. std::thread::Thread is now just std::thread
 */
 
-fn plus_one(sender: &Sender<int>, receiver: &Receiver<int>) {
-  let mut value: int;
+fn plus_one(sender: &Sender<i64>, receiver: &Receiver<i64>) {
+  let mut value: i64;
   loop {
-    println!("waiting on a value...");
     value = receiver.recv().unwrap();
-    println!("plus_one got a value {}.", value.to_string());
-    println!("sending value + 1.");
+    println!("child: got {}", value.to_string());
     sender.send(value + 1).unwrap();
     if value == 0 { break; }
   }
 }
 
 fn main() {
-  println!("Starting program.");
-
   let (from_parent_sender, from_parent_receiver) = channel();
   let (from_child_sender, from_child_receiver) = channel();
 
-  println!("defined channels.");
-
-  Thread::spawn(move || {
+  thread::spawn(move || {
     plus_one(&from_child_sender, &from_parent_receiver);
   });
-
-  println!("test");
 
   from_parent_sender.send(22).unwrap();
   from_parent_sender.send(23).unwrap();
   from_parent_sender.send(24).unwrap();
   from_parent_sender.send(25).unwrap();
 
-  for _ in range(0u, 4) {
+  for _ in 0..4 {
     let answer = from_child_receiver.recv().unwrap();
-    println!("{}", answer.to_string());
+    println!("parent: got {}", answer.to_string());
   }
 }
