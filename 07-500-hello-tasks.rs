e4298ecde92258940b0619b9d0d61dae0d444bdc
Dave Yarwood <dave.yarwood@gmail.com>
2016-12-09 08:35:14 -0500

    update 07 for rust 1.9.0

--- 07-500-hello-tasks.rs
+++ 07-500-hello-tasks.rs
@@ -1,7 +1,16 @@
-use std::thread::Thread;
+/*
+  1.9.0:
+  -----
+  1. `u` is no longer a valid suffix for a numeric literal
+  2. `range` is no longer a thing. now there is a literal range syntax
+  3. unused variables cause a warning; use _
+  4. std::thread::Thread is now just std::thread
+*/
+
+use std::thread;
 
 fn main() {
-  for num in range(0u, 500) {
-    Thread::spawn(move || println!("Hello"));
+  for _ in 0..500 {
+    thread::spawn(move || println!("Hello"));
   }
 }
